55d63fcd102c1dbdf87a8652d3acf44705c4eb88
sansan88 <sandro.scalco@liitu.ch>
2024-06-22 15:35:48 +0200

    implement cache

--- fetch_aircraft_data.py
+++ fetch_aircraft_data.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import requests
 import time
 from datetime import datetime
@@ -49,24 +48,37 @@ def lookup_hex_info(hex_code):
 
     return None
 
-def fetch_aircraft_details(hex_code):
-    url = f"https://api.adsbdb.com/v0/aircraft/{hex_code}"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        return response.json().get('response', {}).get('aircraft', {})
-    except requests.RequestException as e:
-        print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Error fetching aircraft details: {e}")
-        return {}
-def fetch_flight_route(callsign):
+def fetch_flight_route(callsign, cache):
+    if callsign in cache:
+        print(f"get callsign {callsign} from cache")
+        return cache[callsign]
+
     url = f"https://api.adsbdb.com/v0/callsign/{callsign}"
     try:
         response = requests.get(url)
         response.raise_for_status()
-        return response.json().get('response', {}).get('flightroute', {}).get('origin',{})
+        flightroute = response.json().get('response', {}).get('flightroute', {})
+        cache[callsign] = flightroute
+        return flightroute
     except requests.RequestException as e:
         print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Error fetching flight route: {e}")
-        return {}
+        return None
+
+def get_aircraft_details(hex_code, cache):
+    if hex_code in cache:
+        print(f"get hex {hex_code} from cache")
+        return cache[hex_code]
+
+    url = f"https://api.adsbdb.com/v0/aircraft/{hex_code}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        aircraft_details = response.json().get('response', {}).get('aircraft', {})
+        cache[hex_code] = aircraft_details
+        return aircraft_details
+    except requests.RequestException as e:
+        print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Error fetching aircraft details: {e}")
+        return None
 
 def search_flight(data, exact_terms, prefix_terms, icao_ranges, categories):
     if 'aircraft' not in data:
@@ -81,8 +93,7 @@ def search_flight(data, exact_terms, prefix_terms, icao_ranges, categories):
             category = aircraft.get('category')
             geom_rate = aircraft.get('geom_rate', 0)
             altitude_ft = aircraft.get('alt_geom', 30000)
-            if any(term == flight for term in exact_terms) or any(flight.startswith(prefix) for prefix in prefix_terms) and category in categories and geom_rate < -0.1 and altitude_ft < 8000:
-                altitude_ft = aircraft.get('alt_geom', None)
+            if any(term == flight for term in exact_terms) or any(flight.startswith(prefix) for prefix in prefix_terms) and category in categories and geom_rate < -0.1 and altitude_ft < 15000:
                 altitude_m = round(altitude_ft * 0.3048) if altitude_ft is not None else "N/A"
                 details = lookup_hex_info(hex_code)
                 country_info = find_icao_range(hex_code, icao_ranges)
@@ -95,6 +106,9 @@ def main():
     categories = ["A3", "A4", "A5"]
     icao_ranges = load_icao_ranges()
 
+    flight_route_cache = {}
+    aircraft_details_cache = {}
+
     while True:
         data = fetch_aircraft_data()
         if data:
@@ -103,32 +117,39 @@ def main():
             if found_flights:
                 for flight_info in found_flights:
                     flight, altitude_m, hex_code, details, country_info = flight_info
-                    aircraft_details = fetch_aircraft_details(hex_code)
-                    flight_origin = fetch_flight_route(flight)
 
-                    text_top = f"Aircraft Details {flight}"
-                    text_center = {country_info['country']}
-                    text_bottom = "Altitude"
+                    flight_route = fetch_flight_route(flight, flight_route_cache)
+                    if flight_route:
+                        origin = flight_route.get('origin', {}).get('name', 'Unknown')
+                        destination = flight_route.get('destination', {}).get('name', 'Unknown')
+                    else:
+                        origin = "Unknown"
+                        destination = "Unknown"
+
+                    aircraft_details = get_aircraft_details(hex_code, aircraft_details_cache)
+                    if aircraft_details:
+                        aircraft_type = aircraft_details.get('type', 'Unknown')
+                    else:
+                        aircraft_type = "Unknown"
 
                     if details:
                         details_str = f"Registration: {details.get('r')}, ICAO Type: {details.get('t')}, Description: {details.get('desc')}, WTC: {details.get('wtc')}"
-                        text_top = {details.get('t')}
                     else:
                         details_str = ""
 
                     print(f"{timestamp} - Found flight: {flight}, Altitude: {altitude_m} meters, Hex: {hex_code}, Country: {country_info['country']}, {details_str}")
+                    print(f"Aircraft details: {aircraft_type}")
 
-                    if aircraft_details:
-                        print(f"Aircraft details: {aircraft_details.get('manufacturer')} {aircraft_details.get('type')} ")
-                        text_top = f"{aircraft_details.get('manufacturer')} {aircraft_details.get('type')} {aircraft_details.get('registered_owner')}"
-                    if flight_origin:
-                        print(f"Flight route: {flight_origin.get('municipality')}")
-                        text_center = f"{flight_origin.get('iata_code')} {flight_origin.get('name')}"
+                    text_top = f"{timestamp}"
+                    text_center = f"{flight} {altitude_m}m"
+                    text_bottom = f"{origin} -> {destination}"
 
-                    text_bottom = f"{altitude_m}m {altitude_m}m {altitude_m}m"
-                    #text_to_display = f"{details.get('t')} {altitude_m}m {flight}"
-                    process = subprocess.Popen(['python', 'rgbtext.py', '--top', text_top, '--center', text_center, '--bottom', text_bottom])
-                    #process = subprocess.Popen(['python', 'runtext.py', '--text', text_to_display])
+                    # Ensure text is string type
+                    text_top = str(text_top)
+                    text_center = str(text_center)
+                    text_bottom = str(text_bottom)
+
+                    process = subprocess.Popen(['python3', 'rgbtext.py', '--top', text_top, '--center', text_center, '--bottom', text_bottom])
                     time.sleep(6)
                     process.terminate()
                     try:
@@ -138,8 +159,8 @@ def main():
             else:
                 print(f"{timestamp} - No matching flights found")
                 time.sleep(2)
-        time.sleep(2)
+        else:
+            time.sleep(2)
 
 if __name__ == "__main__":
     main()
-
